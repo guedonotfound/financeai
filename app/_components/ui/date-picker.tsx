16400030c720150603bb1bb7a3f0e7d4e49fceb4
guedonotfound <g.guedes.a@hotmail.com>
2025-10-09 14:07:07 -0300

    feat: create add transaction dialog

---
+++ app/_components/ui/date-picker.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import * as React from "react";
+import { ptBR } from "date-fns/locale";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { Button } from "./button";
+import { Calendar } from "./calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
+import { OnSelectHandler } from "react-day-picker";
+
+interface DatePickerProps {
+  value?: Date;
+  onChange?: OnSelectHandler<Date | undefined>;
+}
+
+export const DatePicker = ({ value, onChange }: DatePickerProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          data-empty={!value}
+          className="w-full justify-start text-left font-normal data-[empty=true]:text-muted-foreground"
+        >
+          <CalendarIcon />
+          {value ? (
+            new Date(value).toLocaleDateString("pt-BR", {
+              day: "2-digit",
+              month: "long",
+              year: "numeric",
+            })
+          ) : (
+            <span>Seleciona a data...</span>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={value}
+          onSelect={onChange}
+          locale={ptBR}
+          // hidden={{ after: new Date() }}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
