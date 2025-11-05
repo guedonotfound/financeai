963aca5008c02aa0a3f54116aa7a4b0824e2e8df
guedonotfound <g.guedes.a@hotmail.com>
2025-11-05 14:55:13 -0300

    feat: add initial subscription page component

---
+++ app/subscriptions/page.tsx
@@ -0,0 +1,11 @@
+const SubscriptionPage = () => {
+  return (
+    <>
+      <div className="w-full items-center justify-center py-10 text-center text-5xl">
+        Page being created
+      </div>
+    </>
+  );
+};
+
+export default SubscriptionPage;
