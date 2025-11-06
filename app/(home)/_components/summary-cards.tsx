d71bee47d6d5aa19762e8ccf9031304c5d1b4a7e
guedonotfound <g.guedes.a@hotmail.com>
2025-11-06 16:42:31 -0300

    feat: add summary cards for transaction overview on home page

--- app/(home)/_components/summary-cards.tsx
+++ app/(home)/_components/summary-cards.tsx
@@ -1,5 +1,71 @@
-const SummaryCards = () => {
-  return <></>;
+import {
+  PiggyBankIcon,
+  TrendingDownIcon,
+  TrendingUpIcon,
+  WalletIcon,
+} from "lucide-react";
+import SummaryCard from "./summary-card";
+import { db } from "@/app/_lib/prisma";
+
+const SummaryCards = async () => {
+  const depositsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { type: "DEPOSIT" },
+        _sum: { amount: true },
+      })
+    )?._sum?.amount,
+  );
+  const investmentsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { type: "INVESTMENT" },
+        _sum: { amount: true },
+      })
+    )?._sum?.amount,
+  );
+  const expensesTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { type: "EXPENSE" },
+        _sum: { amount: true },
+      })
+    )?._sum?.amount,
+  );
+
+  const balance = depositsTotal - investmentsTotal - expensesTotal;
+
+  return (
+    <div className="space-y-6">
+      <SummaryCard
+        icon={<WalletIcon size={16} />}
+        title="Saldo"
+        amount={balance}
+        size="large"
+      />
+
+      <div className="grid grid-cols-3 gap-6">
+        <SummaryCard
+          icon={<PiggyBankIcon size={16} />}
+          title="Investido"
+          amount={investmentsTotal}
+          size="small"
+        />
+        <SummaryCard
+          icon={<TrendingUpIcon size={16} className="text-primary" />}
+          title="Receita"
+          amount={depositsTotal}
+          size="small"
+        />
+        <SummaryCard
+          icon={<TrendingDownIcon size={16} className="text-red-500" />}
+          title="Despesa"
+          amount={expensesTotal}
+          size="small"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SummaryCards;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,24 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import EmptyCard from "../transactions/_components/empty-card";
+// import EmptyCard from "../transactions/_components/empty-card";
+import SummaryCards from "./_components/summary-cards";
+// import { db } from "../_lib/prisma";
 
 const Home = async () => {
   const { userId } = auth();
   if (!userId) {
     redirect("/login");
   }
+  // const transactions = await db.transaction.findMany({
+  //   where: {
+  //     userId,
+  //   },
+  // });
   return (
-    <div className="flex items-center justify-center">
-      <EmptyCard />
-    </div>
+    // <div className="flex items-center justify-center">
+    //   {transactions.length > 0 ? <SummaryCards /> : <EmptyCard />}
+    // </div>
+    <SummaryCards />
   );
 };
 
