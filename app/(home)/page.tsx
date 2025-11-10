24d26aa1be03e6cb9cc0fc8c04ae09c5b981e5a1
guedonotfound <g.guedes.a@hotmail.com>
2025-11-10 09:39:01 -0300

    feat: include empty card to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,10 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-// import EmptyCard from "../transactions/_components/empty-card";
+import EmptyCard from "../transactions/_components/empty-card";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
 import { isMatch } from "date-fns";
-// import { db } from "../_lib/prisma";
+import { db } from "../_lib/prisma";
 
 interface HomeProps {
   searchParams: {
@@ -22,23 +22,26 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
     redirect("?month=01");
   }
 
-  // const transactions = await db.transaction.findMany({
-  //   where: {
-  //     userId,
-  //   },
-  // });
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
     <div className="space-y-6 p-6">
-      {/* <div className="flex items-center justify-center">
-    {transactions.length > 0 ? <SummaryCards /> : <EmptyCard />}
-    </div> */}
       <div className="flex justify-between">
         <h1 className="text-2xl font-bold">Dashboard</h1>
         <TimeSelect />
       </div>
-      <div className="grid grid-cols-[2fr,1fr]">
-        <SummaryCards month={month} />
-      </div>
+      {transactions.length > 0 ? (
+        <div className="grid grid-cols-[2fr,1fr]">
+          <SummaryCards month={month} />
+        </div>
+      ) : (
+        <div className="flex w-full items-center">
+          <EmptyCard />
+        </div>
+      )}
     </div>
   );
 };
