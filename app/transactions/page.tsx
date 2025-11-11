d26d031259f2e8a0b34e5b4f421c507763b1b7e1
guedonotfound <g.guedes.a@hotmail.com>
2025-11-11 15:17:19 -0300

    feat: implement user authentication check in dashboard retrieval and update month redirection logic

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -4,6 +4,7 @@ import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { ScrollArea } from "../_components/ui/scroll-area";
 
 const TransactionsPage = async () => {
   const { userId } = auth();
@@ -17,12 +18,14 @@ const TransactionsPage = async () => {
     orderBy: [{ date: "desc" }, { createdAt: "desc" }],
   });
   return (
-    <div className="space-y-6 p-6">
+    <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
         <AddTransactionButton />
       </div>
-      <DataTable columns={transactionColumns} data={transactions} />
+      <ScrollArea>
+        <DataTable columns={transactionColumns} data={transactions} />
+      </ScrollArea>
     </div>
   );
 };
