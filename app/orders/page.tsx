4cd46371eb5d730f680c8b1344c35c4e148da810
guedonotfound <g.guedes.a@hotmail.com>
2025-11-12 10:34:01 -0300

    feat: implement authentication and admin check in OrdersPage component

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,4 +1,16 @@
-const OrdersPage = () => {
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { getAdminUser } from "../_data/get-admin-user";
+
+const OrdersPage = async () => {
+  const userId = auth();
+  const isAdmin = getAdminUser();
+  if (!userId) {
+    redirect("/login");
+  }
+  if (!isAdmin) {
+    throw new Error("Unsauthorized");
+  }
   return (
     <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
       <div className="flex w-full items-center justify-between">
