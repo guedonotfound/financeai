c7d3cb0a2e5c3b9f98fd69499a203399898006ad
guedonotfound <g.guedes.a@hotmail.com>
2025-11-24 06:37:46 -0300

    fix: handle zero values in profit percentage calculation

--- app/_data/get-profit/index.ts
+++ app/_data/get-profit/index.ts
@@ -1,7 +1,14 @@
 import { formatCurrency } from "@/app/_utils/currency";
 
 export const getProfit = (amount: number, costPrice: number) => {
-  const percentage = (((amount - costPrice) / costPrice) * 100).toFixed(2);
+  let percentage: string;
+  if (amount !== 0 && costPrice !== 0) {
+    percentage = (((amount - costPrice) / costPrice) * 100)
+      .toFixed(2)
+      .replace(".", ",");
+  } else {
+    percentage = "0,00";
+  }
 
   const value = formatCurrency(amount - costPrice);
 
